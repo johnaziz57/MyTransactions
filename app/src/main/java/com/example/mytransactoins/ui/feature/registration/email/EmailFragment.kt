7a963380fdf694259d01687bb7523d9735aad45a
John Salib <john.salib@teampicnic.com>
2023-07-14 19:52:44 +0200

    Add Email Fragment

---
+++ app/src/main/java/com/example/mytransactoins/ui/feature/registration/email/EmailFragment.kt
@@ -0,0 +1,17 @@
+package com.example.mytransactoins.ui.feature.registration.email
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.mytransactoins.R
+import com.example.mytransactoins.databinding.FragmentEmailBinding
+import com.example.mytransactoins.ui.utils.viewBinding
+
+class EmailFragment : Fragment(R.layout.fragment_email) {
+    private val binding by viewBinding(FragmentEmailBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+}
