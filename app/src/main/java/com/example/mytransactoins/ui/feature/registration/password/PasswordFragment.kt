097e3681693adb16003bbf8881d4016fb032a17d
John Salib <john.salib@teampicnic.com>
2023-07-14 20:06:50 +0200

    Add PasswordFragment

---
+++ app/src/main/java/com/example/mytransactoins/ui/feature/registration/password/PasswordFragment.kt
@@ -0,0 +1,9 @@
+package com.example.mytransactoins.ui.feature.registration.password
+
+import androidx.fragment.app.Fragment
+import com.example.mytransactoins.databinding.FragmentPasswordBinding
+import com.example.mytransactoins.ui.utils.viewBinding
+
+class PasswordFragment : Fragment() {
+    val binding by viewBinding(FragmentPasswordBinding::bind)
+}
