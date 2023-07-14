7931c0b187ad30c018b465d3c7fca7b4a8a6d47d
John Salib <john.salib@teampicnic.com>
2023-07-14 19:55:54 +0200

    Add email verification fragment

---
+++ app/src/main/java/com/example/mytransactoins/ui/feature/registration/email_verification/EmailVerificationFragment.kt
@@ -0,0 +1,10 @@
+package com.example.mytransactoins.ui.feature.registration.email_verification
+
+import androidx.fragment.app.Fragment
+import com.example.mytransactoins.R
+import com.example.mytransactoins.databinding.FragmentEmailVerificationBinding
+import com.example.mytransactoins.ui.utils.viewBinding
+
+class EmailVerificationFragment : Fragment(R.layout.fragment_email_verification) {
+    val binding by viewBinding(FragmentEmailVerificationBinding::bind)
+}
