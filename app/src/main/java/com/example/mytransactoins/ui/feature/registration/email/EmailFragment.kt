32ae00ba98b9ccbf45d97e9201ba118a3a995c6a
John Salib <john.salib@teampicnic.com>
2023-07-15 11:57:06 +0200

    Add navigation to EmailFragment

--- app/src/main/java/com/example/mytransactoins/ui/feature/registration/email/EmailFragment.kt
+++ app/src/main/java/com/example/mytransactoins/ui/feature/registration/email/EmailFragment.kt
@@ -3,6 +3,7 @@ package com.example.mytransactoins.ui.feature.registration.email
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.mytransactoins.R
 import com.example.mytransactoins.databinding.FragmentEmailBinding
 import com.example.mytransactoins.ui.utils.viewBinding
@@ -11,7 +12,9 @@ class EmailFragment : Fragment(R.layout.fragment_email) {
     private val binding by viewBinding(FragmentEmailBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        binding.buttonRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_emailFragment_to_emailVerificationFragment)
+        }
     }
 
 }
