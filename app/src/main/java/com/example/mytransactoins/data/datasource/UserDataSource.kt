8bf3a8a98fcdc92a32895cbde56b07e7d76e8437
John Salib <john.salib@teampicnic.com>
2023-07-15 17:05:01 +0200

    Remove UserDataSource

--- app/src/main/java/com/example/mytransactoins/data/datasource/UserDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mytransactoins.data.datasource
-
-import com.example.mytransactoins.data.model.EncryptedUserDTO
-
-interface UserDataSource {
-    fun getUser(): EncryptedUserDTO
-    fun addUser(encryptedUserDTO: EncryptedUserDTO)
-}
--- app/src/main/java/com/example/mytransactoins/data/repo/UserRepoImpl.kt
+++ app/src/main/java/com/example/mytransactoins/data/repo/UserRepoImpl.kt
@@ -1,10 +1,9 @@
 package com.example.mytransactoins.data.repo
 
-import com.example.mytransactoins.data.datasource.UserDataSource
 import com.example.mytransactoins.domain.repo.UserRepo
 import javax.inject.Inject
 
-class UserRepoImpl @Inject constructor(private val userDataSource: UserDataSource) : UserRepo {
+class UserRepoImpl @Inject constructor() : UserRepo {
     // TODO add Resource class to contain the result and error if something goes wrong
     override fun getEncryptedPassword(email: String): String {
         return ""// TODO get data from DB
