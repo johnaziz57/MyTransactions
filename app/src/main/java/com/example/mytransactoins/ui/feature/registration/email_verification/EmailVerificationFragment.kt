1dfd4eb7b2d044a33ff386e94f15f5a33f8a2697
John Salib <john.salib@teampicnic.com>
2023-07-15 11:57:20 +0200

    Add navigation to EmailVerificationFragment

--- app/src/main/java/com/example/mytransactoins/ui/feature/registration/email_verification/EmailVerificationFragment.kt
+++ app/src/main/java/com/example/mytransactoins/ui/feature/registration/email_verification/EmailVerificationFragment.kt
@@ -1,10 +1,19 @@
 package com.example.mytransactoins.ui.feature.registration.email_verification
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.mytransactoins.R
 import com.example.mytransactoins.databinding.FragmentEmailVerificationBinding
 import com.example.mytransactoins.ui.utils.viewBinding
 
 class EmailVerificationFragment : Fragment(R.layout.fragment_email_verification) {
-    val binding by viewBinding(FragmentEmailVerificationBinding::bind)
+    private val binding by viewBinding(FragmentEmailVerificationBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.buttonConfirm.setOnClickListener {
+            findNavController().navigate(R.id.action_emailVerificationFragment_to_passwordFragment)
+        }
+    }
 }
