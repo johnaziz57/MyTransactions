20c04019bd4d6964098b81a417bd96651c587241
John Salib <john.salib@teampicnic.com>
2023-07-15 14:15:33 +0200

    Add RegistrationViewModel to PasswordFragment

--- app/src/main/java/com/example/mytransactoins/ui/feature/registration/password/PasswordFragment.kt
+++ app/src/main/java/com/example/mytransactoins/ui/feature/registration/password/PasswordFragment.kt
@@ -3,17 +3,28 @@ package com.example.mytransactoins.ui.feature.registration.password
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.activityViewModels
 import com.example.mytransactoins.R
 import com.example.mytransactoins.databinding.FragmentPasswordBinding
+import com.example.mytransactoins.ui.feature.registration.RegistrationViewModel
 import com.example.mytransactoins.ui.utils.viewBinding
 
 class PasswordFragment : Fragment(R.layout.fragment_password) {
     private val binding by viewBinding(FragmentPasswordBinding::bind)
-
+    private val viewModel: RegistrationViewModel by activityViewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.buttonConfirm.setOnClickListener {
-            findNavController().navigate(R.id.action_passwordFragment_to_transactionFragment)
+        with(binding) {
+            buttonConfirm.setOnClickListener {
+                viewModel.submitPassword(
+                    editTextPassword.text.toString(),
+                    editTextRepeatPassword.text.toString()
+                )
+            }
+            viewModel.validatePasswordLiveData.observe(viewLifecycleOwner) {
+                if (!it.isSuccessful) {
+                    editTextPassword.error = it.message
+                }
+            }
         }
     }
 }
