b50213287db00eed8b271a9eb62636aeb40bf08b
John Salib <john.salib@teampicnic.com>
2023-07-15 11:08:28 +0200

    Move TransactionFragment to ui.feature.transaction

---
+++ app/src/main/java/com/example/mytransactoins/MyTransactionsApplication.kt
@@ -0,0 +1,7 @@
+package com.example.mytransactoins
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyTransactionsApplication : Application()
---
+++ app/src/main/java/com/example/mytransactoins/ui/feature/transaction/TransactionFragment.kt
@@ -0,0 +1,9 @@
+package com.example.mytransactoins.ui.feature.transaction
+
+import androidx.fragment.app.Fragment
+import com.example.mytransactoins.databinding.FragmentTransactionBinding
+import com.example.mytransactoins.ui.utils.viewBinding
+
+class TransactionFragment : Fragment() {
+    val binding by viewBinding(FragmentTransactionBinding::bind)
+}
