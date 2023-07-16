9f0a4c076a0e32a1a3e77e7cc372782b78a14660
John Salib <john.salib@teampicnic.com>
2023-07-16 12:35:59 +0200

    Add RegistrationInteractorImplTest

---
+++ app/src/test/java/com/example/mytransactoins/domain/interactor/register/RegistrationInteractorImplTest.kt
@@ -0,0 +1,32 @@
+package com.example.mytransactoins.domain.interactor.register
+
+import com.example.mytransactoins.domain.model.Result
+import com.example.mytransactoins.domain.repo.UserRepo
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.anyString
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class RegistrationInteractorImplTest {
+    @Mock
+    private lateinit var userRepo: UserRepo
+
+    private lateinit var registrationInteractor: RegistrationInteractorImpl
+
+    @Before
+    fun setup() {
+        registrationInteractor = RegistrationInteractorImpl(userRepo)
+    }
+
+    @Test
+    fun `test registration is successful`() {
+        `when`(userRepo.logIn(anyString(), anyString())).thenReturn(Result(isSuccessful = true))
+        assert(registrationInteractor.registerUser("email", "password").isSuccessful)
+    }
+
+}
