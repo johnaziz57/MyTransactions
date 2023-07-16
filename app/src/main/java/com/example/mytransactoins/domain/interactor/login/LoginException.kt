c147ba0e038eb6839d7f82933adb2142c73de3cd
John Salib <john.salib@teampicnic.com>
2023-07-16 16:13:58 +0200

    Extract PasswordTooShortException to different file

--- app/src/main/java/com/example/mytransactoins/domain/interactor/login/LoginException.kt
+++ app/src/main/java/com/example/mytransactoins/domain/interactor/login/LoginException.kt
@@ -3,6 +3,3 @@ package com.example.mytransactoins.domain.interactor.login
 sealed class LoginException : Throwable()
 class UserDoesNotExistException : LoginException()
 class IncorrectCredentialsException : LoginException()
-
-
-class PasswordTooShortException : Throwable()
---
+++ app/src/main/java/com/example/mytransactoins/domain/interactor/login/PasswordTooShortException.kt
@@ -0,0 +1,3 @@
+package com.example.mytransactoins.domain.interactor.login
+
+class PasswordTooShortException : Throwable()
