e8e13632718269634a9576cb2b434b8a9754b36a
John Salib <john.salib@teampicnic.com>
2023-07-16 18:51:38 +0200

    Add ProgressBar to EntryFragment

--- app/src/main/java/com/example/mytransactoins/ui/feature/entry/EntryFragment.kt
+++ app/src/main/java/com/example/mytransactoins/ui/feature/entry/EntryFragment.kt
@@ -2,6 +2,8 @@ package com.example.mytransactoins.ui.feature.entry
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -18,8 +20,25 @@ class EntryFragment : Fragment(R.layout.fragment_entry) {
     private val viewModel: EntryViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        if (viewModel.isLoggedIn()) {
-            findNavController().navigateToNewTaskActivity(EntryFragmentDirections.actionEntryFragmentToTransactionActivity())
+        binding.progressBarLoading.isVisible = true
+        binding.groupButtons.isVisible = false
+
+        viewModel.isLoggedInLiveData.observe(viewLifecycleOwner) {
+            if (it.isSuccessful) {
+                if (it.data == true) {
+                    findNavController().navigateToNewTaskActivity(EntryFragmentDirections.actionEntryFragmentToTransactionActivity())
+                } else {
+                    stopLoading()
+                }
+            } else {
+                Toast.makeText(
+                    context,
+                    it.errorMessage ?: "Something went wrong",
+                    Toast.LENGTH_LONG
+                ).show()
+                stopLoading()
+            }
+
         }
 
         binding.buttonLogin.setOnClickListener {
@@ -28,5 +47,12 @@ class EntryFragment : Fragment(R.layout.fragment_entry) {
         binding.buttonRegister.setOnClickListener {
             findNavController().navigate(EntryFragmentDirections.actionEntryFragmentToRegistrationActivity())
         }
+
+        view.postDelayed({ viewModel.isLoggedIn() }, 1000)
+    }
+
+    private fun stopLoading() {
+        binding.groupButtons.isVisible = true
+        binding.progressBarLoading.isVisible = false
     }
 }
--- app/src/main/java/com/example/mytransactoins/ui/feature/entry/EntryViewModel.kt
+++ app/src/main/java/com/example/mytransactoins/ui/feature/entry/EntryViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.mytransactoins.ui.feature.entry
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.mytransactoins.domain.interactor.entry.EntryInteractor
+import com.example.mytransactoins.ui.feature.mode.LiveDataResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,7 +12,11 @@ import javax.inject.Inject
 class EntryViewModel @Inject constructor(
     private val entryInteractor: EntryInteractor
 ) : ViewModel() {
-    fun isLoggedIn(): Boolean {
-        return entryInteractor.isLoggedIn()
+    val isLoggedInLiveData: LiveData<LiveDataResult<Boolean>>
+        get() = _isLoggedIn
+
+    private val _isLoggedIn = MutableLiveData<LiveDataResult<Boolean>>()
+    fun isLoggedIn() {
+        _isLoggedIn.value = LiveDataResult(isSuccessful = true, data = entryInteractor.isLoggedIn())
     }
 }
