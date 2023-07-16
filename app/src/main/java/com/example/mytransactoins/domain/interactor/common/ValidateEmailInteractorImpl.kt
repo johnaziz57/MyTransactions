9d7cafb454eb5f0a0e10e64cec3d99e8cfeb20ce
John Salib <john.salib@teampicnic.com>
2023-07-16 11:37:45 +0200

    Add ValidateEmailInteractor to LoginInteractor

--- app/src/main/java/com/example/mytransactoins/domain/interactor/common/ValidateEmailInteractorImpl.kt
+++ app/src/main/java/com/example/mytransactoins/domain/interactor/common/ValidateEmailInteractorImpl.kt
@@ -1,11 +1,19 @@
 package com.example.mytransactoins.domain.interactor.common
 
+import android.util.Patterns
 import com.example.mytransactoins.domain.model.Result
-import com.example.mytransactoins.domain.utils.Utils
 import javax.inject.Inject
 
 class ValidateEmailInteractorImpl @Inject constructor() : ValidateEmailInteractor {
     override fun validateEmail(email: String): Result {
-        return Utils.validateEmail(email)
+        if (email.isBlank()) {
+            return Result(isSuccessful = false, message = "email is empty")
+        }
+
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches().not()) {
+            return Result(isSuccessful = false, message = "email is invalid")
+        }
+
+        return Result(isSuccessful = true)
     }
 }
--- app/src/main/java/com/example/mytransactoins/domain/interactor/login/LoginInteractorImpl.kt
+++ app/src/main/java/com/example/mytransactoins/domain/interactor/login/LoginInteractorImpl.kt
@@ -1,17 +1,18 @@
 package com.example.mytransactoins.domain.interactor.login
 
+import com.example.mytransactoins.domain.interactor.common.ValidateEmailInteractor
 import com.example.mytransactoins.domain.model.Result
 import com.example.mytransactoins.domain.repo.UserRepo
 import com.example.mytransactoins.domain.utils.Constants
-import com.example.mytransactoins.domain.utils.Utils
 import javax.inject.Inject
 
 class LoginInteractorImpl @Inject constructor(
-    private val userRepo: UserRepo
+    private val userRepo: UserRepo,
+    private val validateEmailInteractor: ValidateEmailInteractor
 ) : LoginInteractor {
 
     override fun validateEmail(email: String): Result {
-        return Utils.validateEmail(email)
+        return validateEmailInteractor.validateEmail(email)
     }
 
     override fun validatePasswordLength(password: String): Result {
